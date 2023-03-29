a8b2473647d09809cf01606eb7b8d889e867b95f
Amanda Zanata <amandazanata46@gmail.com>
2023-03-29 20:46:44 -0300

    fix: Requisitos 9 e 10

---
+++ desafio{11}.sql
@@ -0,0 +1,5 @@
+--  Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de 
+-- Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39.
+
+-- Dica: Purchase generated based on Order é um valor atribuído à coluna 
+-- notes e não uma coluna.
