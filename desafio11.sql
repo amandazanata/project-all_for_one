1cddaf754add65a7e9851929b7029fab0830521f
Amanda Zanata <amandazanata46@gmail.com>
2023-03-30 17:46:03 -0300

    fix: Requisito 4 a testa requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,6 @@
---  Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de 
--- Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39.
+SELECT * FROM purchase_orders
+WHERE notes LIKE '%purchase%' AND '3_';
 
--- Dica: Purchase generated based on Order é um valor atribuído à coluna 
--- notes e não uma coluna.
+
+-- Exiba os dados da coluna notes da tabela purchase_orders em que seu valor de 
+-- Purchase generated based on Order é maior ou igual a 30 e menor ou igual a 39.
--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(product_name) FROM products;
+SELECT COUNT(*) FROM products;
 
 --Conte quantos registros existem na coluna product_name da tabela products.
