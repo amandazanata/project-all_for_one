da3598dfe8e59c95546c8f3a6c136379e8ffa276
Amanda Zanata <amandazanata46@gmail.com>
2023-04-03 21:00:23 -0300

    requisito 21

--- desafio20.sql
+++ desafio20.sql
@@ -5,7 +5,3 @@ VALUE (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129)
 -- 'product_id': 80, 'quantity': 15.0000, 'unit_price': 15.0000,
 -- 'discount': 0, 'status_id': 2, 'date_allocated': NULL, 
 -- 'purchase_order_id': NULL e 'inventory_id': 129
-
--- Dica: O id deve ser incrementado automaticamente.
--- Para entender melhor isso, você pode consultar o arquivo de criação da tabela
--- (./northwind.sql, na linha 439) aqui.
